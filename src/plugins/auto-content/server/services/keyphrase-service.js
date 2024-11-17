142e768ca90037713799ea880b621c5f08b0d662
theannachen <annachen031014@gmail.com>
2024-11-16 18:31:03 -0600

    Update keyphrase-service.js

--- src/plugins/auto-content/server/services/keyphrase-service.js
+++ src/plugins/auto-content/server/services/keyphrase-service.js
@@ -10,10 +10,11 @@ module.exports = ({ strapi }) => {
       const prompt = [
         {
           role: "user",
-          content:`You will generate 3 to 5 keyphrases from the provided passage. The passage comes from an educational text, and the keyphrases should represent important concepts in the passage. A good keyphrase will be concise, and will likely be a noun phrase, like "patent infringement" or "epistemological belief." A bad keyphrase will be wordy and vague, like "extend the idea of scope," or will end mid-sentence like "defective condition unreasonably dangerous." Make sure the keyphrases do not overlap. For example, "sale" and "contract for sale" should not both be provided as keyphrases. Return the result as a single line of the values each separated by a comma.`,
+          content:`You will generate 3 to 5 keyphrases from the provided passage. The passage comes from an educational text, and the keyphrases should represent important concepts in the passage. A good keyphrase will be concise, and will likely be a noun phrase, like "patent infringement" or "epistemological belief." A bad keyphrase will be wordy and vague, like "extend the idea of scope," or will end mid-sentence like "defective condition unreasonably dangerous." Make sure the keyphrases do not overlap. For example, "sale" and "contract for sale" should not both be provided as keyphrases. Return the result as a single line of the values each separated by a comma.`+
             text,
         },
       ];
+      console.log(text)
       const response = await fetch(
         `https://api.openai.com/v1/chat/completions`,
         {
