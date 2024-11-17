0522253c174191f908da93f7cc65a7757197d1aa
theannachen <annachen031014@gmail.com>
2024-11-17 12:29:16 -0600

    fixed validation error

--- src/extensions/validations.js
+++ src/extensions/validations.js
@@ -1,17 +1,18 @@
 "use strict";
-const { ApplicationError } = require("@strapi/utils");
+const { errors } = require('@strapi/utils');
+const { ApplicationError } = errors;
 const yup = require("yup");
 
 function validateKeyPhraseField(event) {
   const { data } = event.params;
-  const keyphrase_string = data.KeyPhrase ? data.KeyPhrase : "[]";
+  const keyphrase_string = data.KeyPhrase ? data.KeyPhrase : "";
   try {
-    if(keyphrase_string.search(/^((\w| |\n)+,)+((\w| |\n)+)$/g) === -1){
+    if(keyphrase_string.length > 0 && keyphrase_string.search(/^((\w| |\n)+,)+((\w| |\n)+)$/g) === -1){
       throw new Error('Invalid Keyphrase format');
     }
   } catch {
     throw new ApplicationError(
-      `Header: ${data.Header}\nKeyphrase Parsing Error: ${keyphrase_string}`,
+      `Keyphrase Parsing Error: ${keyphrase_string}`,
     );
   }
 }
